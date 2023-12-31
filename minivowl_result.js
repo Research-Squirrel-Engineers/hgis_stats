2fa65ef6baa8ee7a37c8f6a13d66b1e696a434f6
situx <situx@users.noreply.github.com>
2023-12-31 18:02:11 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/hgis_stats@f4d9e4bec075fa56f51aa059f6de221796bcdf07 🚀

---
+++ minivowl_result.js
@@ -0,0 +1,105 @@
+var minivowlresult={
+ "info": [
+  {
+   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
+  }
+ ],
+ "nodes": [
+  {
+   "name": "DatatypeProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#DatatypeProperty"
+  },
+  {
+   "name": "ObjectProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
+  },
+  {
+   "name": "ObservationCollection",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/sosa/ObservationCollection"
+  },
+  {
+   "name": "Observation",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/sosa/Observation"
+  },
+  {
+   "name": "Instant",
+   "type": "class",
+   "uri": "http://www.w3.org/2006/time#Instant"
+  },
+  {
+   "name": "Platform",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/sosa/Platform"
+  },
+  {
+   "name": "Person",
+   "type": "class",
+   "uri": "http://xmlns.com/foaf/0.1/Person"
+  },
+  {
+   "name": "NamedIndividual",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#NamedIndividual"
+  },
+  {
+   "name": "ObservableProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/ns/sosa/ObservableProperty"
+  }
+ ],
+ "links": [
+  {
+   "source": 2,
+   "target": 3,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 3,
+   "target": 4,
+   "valueTo": "phenomenonTime",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/phenomenonTime"
+  },
+  {
+   "source": 3,
+   "target": 8,
+   "valueTo": "observedProperty",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
+  },
+  {
+   "source": 3,
+   "target": 5,
+   "valueTo": "hasFeatureOfInterest",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/hasFeatureOfInterest"
+  },
+  {
+   "source": 5,
+   "target": 6,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/creator"
+  },
+  {
+   "source": 2,
+   "target": 6,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/creator"
+  },
+  {
+   "source": 5,
+   "target": 2,
+   "valueTo": "hasObservation",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/hasObservation"
+  }
+ ]
+}
